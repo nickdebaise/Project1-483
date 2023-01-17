3ffbbed8d6dec51b5a22608df828c8e0ee53e784
Nick DeBaise <nickdebaise@gmail.com>
2023-01-17 10:34:58 -0500

    update prob for interpolation

--- ngram_lm.py
+++ ngram_lm.py
@@ -93,20 +93,19 @@ class NgramModel(object):
             return 1 / len(self.vocab)
         else:
             n = (context, char)
+            # add-k smoothing
             return (self.ngrams.get(str(n), 0) + self.k) / (count + (self.k * len(self.get_vocab())))
-
-        pass
 
     def random_char(self, context):
         ''' Returns a random character based on the given context and the 
             n-grams learned by this model '''
         r = random.random()
 
-        sum = 0
+        sum_prob = 0
         for j in sorted(self.vocab):
-            sum += self.prob(context, j)
+            sum_prob += self.prob(context, j)
 
-            if r < sum:
+            if r < sum_prob:
                 return j
 
     def random_text(self, length):
@@ -126,7 +125,7 @@ class NgramModel(object):
         ''' Returns the perplexity of text based on the n-grams learned by
             this model '''
         N = len(text)
-        sum = 0
+        sum_log = 0
         grams = ngrams(self.c, text)
 
         for gram in grams:
@@ -135,11 +134,9 @@ class NgramModel(object):
             if p == 0:
                 return float('inf')
 
-            sum += math.log2(p)
+            sum_log += math.log2(p)
 
-        return 2 ** (-1 / N * sum)
-
-
+        return 2 ** (-1 / N * sum_log)
 
 
 ################################################################################
@@ -150,16 +147,63 @@ class NgramModelWithInterpolation(NgramModel):
     ''' An n-gram model with interpolation '''
 
     def __init__(self, c, k):
-        pass
-
-    def get_vocab(self):
-        pass
+        super().__init__(c, k)
+        self.weights = [1 / (self.c + 1) for _ in range(self.c + 1)]
 
     def update(self, text):
-        pass
+        for c in range(self.c + 1):
+            grams = ngrams(c, text)
+
+            for gram in grams:
+                self.vocab.add(gram[1])
+
+                if self.ngrams.get(str(gram)):
+                    self.ngrams[str(gram)] += 1
+                else:
+                    self.ngrams[str(gram)] = 1
+
+    def get_prob(self, context, char):
+        count = 0
+
+        for ch in self.get_vocab():
+            n = (context, ch)
+            if self.ngrams.get(str(n)):
+                count += self.ngrams.get(str(n))
+
+        if count == 0:
+            return 1 / len(self.vocab)
+        else:
+            n = (context, char)
+            # add-k smoothing
+            return (self.ngrams.get(str(n), 0) + self.k) / (count + (self.k * len(self.get_vocab())))
 
     def prob(self, context, char):
-        pass
+        """
+        P(Wn|Wn-2Wn-1) = lP(Wn) + lP(Wn|Wn-1) + lP(Wn|Wn-2Wn-1)
+        """
+        total_prob = 0
+
+        for c in range(len(context) + 1):
+            weight = self.weights[c]
+            p = self.get_prob(context[:c], char)
+
+            total_prob += weight * p
+        return total_prob
+
+
+m = NgramModelWithInterpolation(1, 0)
+m.update('abab')
+
+print(m.prob('a', 'a'))
+print(m.prob('a', 'b'))
+
+# t = NgramModelWithInterpolation(2, 1)
+# t.update('abab')
+# t.update('abcd')
+#
+# print(t.prob('~a', 'b'))
+# print(t.prob('ba', 'b'))
+
 
 ################################################################################
 # Your N-Gram Model Experimentations
@@ -172,19 +216,30 @@ class NgramModelWithInterpolation(NgramModel):
 # Hint: it may be useful to encapsulate it into multiple functions so
 # that you can easily run any test or experiment at any time.
 
-# m = create_ngram_model(NgramModel, 'shakespeare_input.txt', 6, 0.00001)
+
+def assert_equals(v1, v2, message):
+    print("Asserting " + str(v1) + " is equal to " + str(v2))
+    assert str(v1) == str(v2), message
+
+#
+# m = create_ngram_model(NgramModel, 'shakespeare_input.txt', 4)
 # print(m.random_text(500))
 #
-m = NgramModel(1, 0)
-m.update('abab')
-print(m.get_vocab())
-m.update('abcd')
-print(m.get_vocab())
-print(m.prob('a', 'a'))
+# print("--------")
 #
-print(m.prob('a', 'b'))
-print(m.prob('~', 'c'))
-print(m.prob('b', 'c'))
-random.seed(1)
-print([m.random_char('') for i in range(25)])
-print(m.perplexity('abcda'))
+# m = NgramModel(1, 0)
+# m.update('abab')
+# assert_equals("['a', 'b']", m.get_vocab(), 'vocab is equal')
+# m.update('abcd')
+# assert_equals("['a', 'b', 'c', 'd']", m.get_vocab(), 'vocab is equal')
+# assert_equals("0.0", m.prob('a', 'a'), 'probability is 0')
+#
+# #
+# assert_equals("1.0", m.prob('a', 'b'), "probability is 1.0")
+# assert_equals("0.0", m.prob('~', 'c'), "probability is 0.0")
+# assert_equals("0.5", m.prob('b', 'c'), "probability is 0.5")
+#
+# random.seed(1)
+# assert_equals("['a', 'd', 'd', 'b', 'b', 'b', 'c', 'd', 'a', 'a', 'd', 'b', 'd', 'a', 'b', 'c', 'a', 'd', 'd', 'a', "
+#               "'a', 'c', 'd', 'b', 'a']", [m.random_char('') for i in range(25)], 'first 25 random characters same')
+# assert_equals("1.5157165665103982", m.perplexity('abcda'), 'perplexity same')
